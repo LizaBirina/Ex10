03edcf28a9c4970e8404a614dbfb1e915a76c1aa
Liza Birina <esbirina@edu.hse.ru>
2021-03-15 22:39:52 +0300

    popitka 5

--- include/postfix.h
+++ include/postfix.h
@@ -1,6 +1,6 @@
 // Copyright 2021 by Liza
 #ifndef INCLUDE_POSTFIX_H_
-#define INCLUDE_PREFIX_H_
+#define INCLUDE_POSTFIX_H_
 #include <string>
 std::string infix2prefix(std::string infix);
 #endif  // INCLUDE_POSTFIX_H_
